524847336b5d0795710d58a698a61ba80bcc8459
Ryan Mao <roadmap171717@gmail.com>
2025-07-31 02:14:41 +0000

    page.tsx:4  Server   Error: Route "/game/[gameId]" used `params.gameId`.

--- src/app/game/[gameId]/page.tsx
+++ src/app/game/[gameId]/page.tsx
@@ -1,5 +1,5 @@
 import { GameRoom } from "@/components/GameRoom";
 
-export default function GamePage({ params }: { params: { gameId: string } }) {
+export default async function GamePage({ params }: { params: { gameId: string } }) {
   return <GameRoom gameId={params.gameId} />;
 }
